2ba0f3dacf2b3d5e4a06d9a462dc0cc1b86e00b8
Siarhei Karol <siarhei_karol@epam.com>
2023-03-16 14:09:20 +0500

    add filter policy

--- import-service/functions/catalogBatchProcess.ts
+++ import-service/functions/catalogBatchProcess.ts
@@ -7,22 +7,24 @@ import { parseProduct } from "utils/parseProduct";
 
 export const catalogBatchProcess = async (event: SQSEvent) => {
   try {
-    const productsList = [];
-
     for await (const record of event.Records) {
       const parsedRecord: IncomingProduct = JSON.parse(record.body);
       const parsedProduct = parseProduct(parsedRecord);
 
       await ProductsService.create(parsedProduct);
 
-      productsList.push(parsedRecord);
+      await SnsService.publish({
+        Subject: "New products added",
+        Message: JSON.stringify(parsedProduct),
+        MessageAttributes: {
+          title: {
+            DataType: "String",
+            StringValue: parsedProduct.title,
+          },
+        },
+        TopicArn: process.env.SNS_ARN,
+      });
     }
-
-    await SnsService.publish({
-      Subject: "New products added",
-      Message: JSON.stringify(productsList),
-      TopicArn: process.env.SNS_ARN,
-    });
   } catch (error) {
     console.error("catalogBatchProcess error", error);
 
